033058c8afb75cb1725652ad045a5c2cc89c971d
amlane <twelvexstring@gmail.com>
2019-05-16 16:11:15 -0500

    it's working gif

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -9,6 +9,12 @@ export const login = creds => dispatch => {
     dispatch({ type: LOGIN_START });
     return axios
     .post('http://localhost:5000/api/login', creds)
-    .then( res => console.log(res) )
+    .then( res => {
+        localStorage.setItem(
+            "token",
+            res.data.payload,
+            dispatch({ type: LOGIN_SUCCESSFUL, payload: res.data.payload })
+        )
+    } )
     .catch( err => console.log(err) )
 }
